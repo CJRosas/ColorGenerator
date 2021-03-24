da01db6dbc1bb0321e4b2bacf1bb0ef2d73e1215
CJRosas <cjrosas.dev@gmail.com>
2021-03-24 17:42:01 -0500

    Added hex color code display

--- ColorGenerator/ViewController.swift
+++ ColorGenerator/ViewController.swift
@@ -26,20 +26,67 @@ extension UIColor {
     }
 }
 
+extension UIColor {
+    
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ColorInfoDisplay: UILabel!
+    
+    var txt = " "
+    var color = UIColor.random()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.view.backgroundColor = .random()
+        txt = color.toHexString().uppercased()
+        txt.remove(at: txt.startIndex)
+        showText()
     }
     
     
     
     @IBAction func changeColor(_ sender: Any) {
-        self.view.backgroundColor = .random()
+        color = .random()
+        self.view.backgroundColor = color
+        txt = color.toHexString().uppercased()
+        txt.remove(at: txt.startIndex)
+        showText()
     }
     
-
+    func showText() {
+        ColorInfoDisplay.text = txt
+    }
+    
+    
 }
 
