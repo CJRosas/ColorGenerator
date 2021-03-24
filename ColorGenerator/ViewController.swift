020f08832ee3e01300a59bb84315f5ed21321081
CJRosas <cjrosas.dev@gmail.com>
2021-03-24 18:10:35 -0500

    Cleaned up code, added comments

--- ColorGenerator/ViewController.swift
+++ ColorGenerator/ViewController.swift
@@ -7,15 +7,16 @@
 
 import UIKit
 
-// Produces random CGFloats in the range 0 to 1
+
 extension CGFloat {
+    // Produces random CGFloats in the range 0 to 1
     static func random() -> CGFloat {
         return CGFloat(arc4random()) / CGFloat(UInt32.max)
     }
 }
 
-// Generates random color
 extension UIColor {
+    //Random color generator
     static func random() -> UIColor {
         return UIColor(
            red:   .random(),
@@ -24,10 +25,8 @@ extension UIColor {
            alpha: 1.0
         )
     }
-}
-
-extension UIColor {
     
+    //Extrapolates the hex number of a UIColor
     convenience init(hex: String, alpha: CGFloat = 1.0) {
         var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
 
@@ -46,6 +45,7 @@ extension UIColor {
                   alpha: alpha)
     }
     
+    //Formats the hex number of a UIColor into a displayable string
     func toHexString() -> String {
         var r:CGFloat = 0
         var g:CGFloat = 0
@@ -59,22 +59,26 @@ extension UIColor {
 
 class ViewController: UIViewController {
     
+    //Label: Displays the hex number of the app background color
     @IBOutlet weak var ColorInfoDisplay: UILabel!
     
+    //Text for the ColorInfoDisplay label
     var txt = " "
+    
+    //Random color variable for app background
     var color = UIColor.random()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .random()
+        
+        self.view.backgroundColor = color
         txt = color.toHexString().uppercased()
         txt.remove(at: txt.startIndex)
         showText()
     }
     
-    
-    
+    //On screen tap, background color changes and the new hex is displayed
     @IBAction func changeColor(_ sender: Any) {
         color = .random()
         self.view.backgroundColor = color
@@ -83,6 +87,7 @@ class ViewController: UIViewController {
         showText()
     }
     
+    //Helper function, sets text in ColorInfoDisplay label
     func showText() {
         ColorInfoDisplay.text = txt
     }
